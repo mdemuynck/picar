4fbc1eb0451dae1c005a6f10147ec05d190230d4
mdemuynck <maarten.demuynck@loqutus.com>
2017-04-06 22:54:37 +0200

    fix

--- 4WD.js
+++ 4WD.js
@@ -9,7 +9,7 @@ stdin.addListener("data", function(d) {
     var pin = gpio.export(d, {
         direction: "out"
     });
-    d.set(1);
+    pin.set(1);
     console.log("you entered: [" +
     d.toString().trim() + "]");
   });
