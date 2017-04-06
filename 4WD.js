a4f6a218dfcbcf76f981f48c74f476ac08d2af69
mdemuynck <maarten.demuynck@loqutus.com>
2017-04-06 22:49:40 +0200

    test

--- 4WD.js
+++ 4WD.js
@@ -1,64 +1,15 @@
 var gpio = require("gpio");
-//Rechts voor
-var ENRV = gpio.export(14, {
-    direction: "out"
-});
-var IN1RV = gpio.export(15, {
-    direction: "out"
-});
-var IN2RV = gpio.export(18, {
-    direction: "out"
-});
-//Rechts achter
-var ENRA = gpio.export(25, {
-    direction: "out"
-});
-var IN1RA = gpio.export(8, {
-    direction: "out"
-});
-var IN2RA = gpio.export(7, {
-    direction: "out"
-});
-//Rechts voor
-var ENLV = gpio.export(17, {
-    direction: "out"
-});
-var IN1LV = gpio.export(27, {
-    direction: "out"
-});
-var IN2LV = gpio.export(22, {
-    direction: "out"
-});
-//Rechts achter
-var ENLA = gpio.export(10, {
-    direction: "out"
-});
-var IN1LA = gpio.export(9, {
-    direction: "out"
-});
-var IN2LA = gpio.export(11, {
-    direction: "out"
-});
-//distance sensor
-var trigger = gpio.export(2, {
-    direction: "out"
-});
-var echo = gpio.export(3, {
-    direction: "out"
-});
 
-//set enables low
-ENRA.set(1);
-ENRV.set(1);
-ENLA.set(0);
-ENLV.set(0);
+
+
 
 var stdin = process.openStdin();
 
 stdin.addListener("data", function(d) {
-    // note:  d is an object, and when converted to a string it will
-    // end with a linefeed.  so we (rather crudely) account for that
-    // with toString() and then trim()
+    var pin = gpio.export(d, {
+        direction: "out"
+    });
+    d.set(1);
     console.log("you entered: [" +
-        d.toString().trim() + "]");
+    d.toString().trim() + "]");
   });
