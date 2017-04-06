19b478982b38c1fc213550788930c23035fc1595
mdemuynck <maarten.demuynck@loqutus.com>
2017-04-06 22:40:39 +0200

    add read from console

--- 4WD.js
+++ 4WD.js
@@ -52,3 +52,13 @@ ENRA.set(1);
 ENRV.set(1);
 ENLA.set(0);
 ENLV.set(0);
+
+var stdin = process.openStdin();
+
+stdin.addListener("data", function(d) {
+    // note:  d is an object, and when converted to a string it will
+    // end with a linefeed.  so we (rather crudely) account for that
+    // with toString() and then trim()
+    console.log("you entered: [" +
+        d.toString().trim() + "]");
+  });
