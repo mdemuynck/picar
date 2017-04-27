474711c5ffe97b8bc1a6276535799014df5c516c
mdemuynck <maarten.demuynck@loqutus.com>
2017-04-27 22:18:21 +0200

    try

--- 4WD.js
+++ 4WD.js
@@ -1,23 +1,37 @@
-var Gpio = require('onoff').Gpio,
-  led = new Gpio(2, 'out');
+var Gpio = require('onoff').Gpio;
+var pins=[];
+//config
+  var EN_RV = new Gpio(14, 'out');
+  pins.push(EN_RV);
+  var IN1_RV = new Gpio(15, 'out');
+  var IN2_RV = new Gpio(18, 'out');
 
-var on = true;
+  var EN_RA = new Gpio(25, 'out');
+  var IN1_RA = new Gpio(8, 'out');
+  var IN2_RA = new Gpio(7, 'out');
+
+  var EN_LA = new Gpio(10, 'out');
+  var IN1_LA = new Gpio(9, 'out');
+  var IN2_LA = new Gpio(11, 'out');
+
+  var EN_LV = new Gpio(17, 'out');
+  var IN1_LV = new Gpio(27, 'out');
+  var IN2_LV = new Gpio(22, 'out');
+
+  var trigger = new Gpio(2, 'out');
+  var echo = new Gpio(3, 'in');
+
+var on = false;
 var stdin = process.openStdin();
-
-
 stdin.addListener("data", function(d) {
-    if (d.indexOf("p") == -1){
-        on = !on;
-        if (on){
-            led.writeSync(1);
-        }else{
-            led.writeSync(0)
-        }
-        console.log("pin is on: " + on);
+
+    on = !on;
+    if (on){
+       led.writeSync(1);
     }else{
-        stdin.addListener("data", function(p){
-            led = new Gpio(p, 'out');
-            console.log("pin set to: " + typeof p)
-        });
+       led.writeSync(0);
+       led = pins.pop();
     }
+    console.log("pin is on: " + on);
+
   });
