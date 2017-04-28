c485287654163c9050cf6361c5c0577ac07013de
mdemuynck <maarten.demuynck@loqutus.com>
2017-04-28 21:43:42 +0200

    enable RV

--- 4WD.js
+++ 4WD.js
@@ -44,6 +44,10 @@ process.stdin.on("keypress", function(ch, key) {
         IN1_RV.writeSync(1);
         IN2_RV.writeSync(0);
         EN_RV.writeSync(1);
+    }else if (key.name == "down"){
+        IN1_RV.writeSync(0);
+        IN2_RV.writeSync(1);
+        EN_RV.writeSync(1);
     }else{
         EN_RV.writeSync(0);
     }
